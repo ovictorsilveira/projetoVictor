85c5eee112eb95b2be11b9f2b6f430a648bf18ca
ovictorsilveira <ovictorsilveira7@gmail.com>
2021-11-15 12:08:00 -0300

    criação de rotas, componentes login e cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CadastroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
